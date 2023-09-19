28692c34e68e9111576c319434b1450610a261ca
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-09-19 11:22:09 +0300

    finish hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,9 @@
 
 import { BtnPrimary } from "../BtnPrimary/BtnPrimary";
 import dynamic from "next/dynamic";
-import styles from "./Hero.module.css";
 import { OfferText } from "../OfferText/OfferText";
+
+import styles from "./Hero.module.css";
 
 const ScrollCarousel = dynamic(
   () => import("@/components/PopCities/PopCities"),
@@ -15,17 +16,12 @@ export const Hero = () => {
     <section className={styles.section}>
       <OfferText />
       <div className={styles.container}>
-        <h1 className={styles.title}>Relocating to Cyprus</h1>
-        <p className={styles.descTitle}>Seamless – Exceptional – Experience.</p>
-        <p className={styles.desc}>
-          The captivating Mediterranean island of Cyprus has become a business
-          haven with a flourishing expat community. The strategic location that
-          grants networking opportunities and the stability of being in the EU
-          offers entrepreneurs the abilities they need to thrive.
-        </p>
+        <h1 className={styles.title}></h1>
+        <p className={styles.descTitle}></p>
+        <p className={styles.desc}></p>
         <BtnPrimary text="Find Out More" color="light" />
       </div>
-      {/* <ScrollCarousel /> */}
+      <ScrollCarousel />
     </section>
   );
 };
